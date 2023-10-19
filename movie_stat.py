f11a750b807139c0c1dd5809c1ff6bb3ac9c89d5
Cem802 <ckirciltepeli@gmail.com>
2023-10-19 20:02:23 +0200

    refactored the function into multiple functions with the extract method #2

--- movie_stat.py
+++ movie_stat.py
@@ -1,3 +1,27 @@
+def calculate_rental_amount(price_code, days_rented):
+    rental_amount = 0
+    
+    if price_code == 0:
+        rental_amount += 2
+        if days_rented > 2:
+            rental_amount += (days_rented - 2) * 1.5
+    elif price_code == 1:
+        rental_amount += days_rented * 3
+    else:
+        rental_amount += 1.5
+        if days_rented > 3:
+            rental_amount += (days_rented - 3) * 1.5
+
+    return rental_amount
+
+def calculate_frequent_renter_points(price_code, days_rented):
+    points = 1
+
+    if price_code == 1 and days_rented > 1:
+        points += 1
+    
+    return points
+
 def statement(name, rentals):
     """
     Generates a rental statement for a customer.
@@ -21,30 +45,17 @@ def statement(name, rentals):
     result = f"Rental Record for {name}\n"
 
     for rental in rentals:
-        this_amount = 0
         movie = rental["movie"]
         days_rented = rental["days_rented"]
 
         # determine amounts for each line
-        if movie["price_code"] == 0:
-            this_amount += 2
-            if days_rented > 2:
-                this_amount += (days_rented - 2) * 1.5
-        elif movie["price_code"] == 1:
-            this_amount += days_rented * 3
-        else:
-            this_amount += 1.5
-            if days_rented > 3:
-                this_amount += (days_rented - 3) * 1.5
+        this_amount = calculate_rental_amount(movie["price_code"], days_rented)
+        
         if False:
             print("This code should never be executed")
 
         # add frequent renter points
-        frequent_renter_points += 1
-
-        # add bonus for a two day new release rental
-        if movie["price_code"] == 1 and days_rented > 1:
-            frequent_renter_points += 1
+        frequent_renter_points += calculate_frequent_renter_points(movie["price_code"], days_rented)
 
         # show figures for this rental
         result += f"\t{movie['title']}\t{this_amount}\n"
